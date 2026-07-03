ad882b26def2b2042e558ccad00c76988a445ba7
Asapgiri <korcsak.gergely@gmail.com>
2026-07-03 15:45:04 +0900

    fix add word in react

--- web/src/Index.tsx
+++ web/src/Index.tsx
@@ -5,12 +5,40 @@ import "./index.css"
 import { WordDB } from "./db/words";
 import { Filter, FilterApply, raw_filter } from "./Filter";
 import { syncFinished } from "./db/sync";
+import { apiFetch } from "./api";
 
 export default function Index() {
     const [words, setWords] = useState<Word[]>([]);
     const [selectedWord, setSelectedWord] = useState<Word | null>(null);
     const [stats, setStats] = useState<Stats | null>(null);
     const [filter, setFilter] = useState<TFilter>(raw_filter);
+    const [addKanji, setAddKanji] = useState<string>("")
+    const [addKana, setAddKana] = useState<string>("")
+    const [addMeaning, setAddMeaning] = useState<string>("")
+
+    async function new_word() {
+        const sbody = {
+            kanji: addKanji,
+            kana: addKana,
+            meaning: addMeaning,
+        }
+
+        const response = await apiFetch(`/api/word`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(sbody)
+        })
+
+        const data = await response.json()
+        console.log(data)
+        setWords(words => [data, ...words])
+
+        setAddKanji("")
+        setAddKana("")
+        setAddMeaning("")
+    }
 
     useEffect(() => {
         loadWords().then(async() => await syncFinished).then(() => loadWords());
@@ -51,16 +79,16 @@ export default function Index() {
             <div className="add-form">
                 <div className="row g-2">
                     <div className="col-md-3">
-                        <input id="form[kanji]" name="form[kanji]" className="form-control clean-input" placeholder="Word (with kanjis)" lang="ja"/>
+                        <input value={addKanji} onChange={e => setAddKanji(e.target.value)} id="form[kanji]" name="form[kanji]" className="form-control clean-input" placeholder="Word (with kanjis)" lang="ja"/>
                     </div>
                     <div className="col-md-3">
-                        <input id="form[kana]" name="form[kana]" className="form-control clean-input" placeholder="Kana" lang="ja"/>
+                        <input value={addKana} onChange={e => setAddKana(e.target.value)} id="form[kana]" name="form[kana]" className="form-control clean-input" placeholder="Kana" lang="ja"/>
                     </div>
                     <div className="col-md-4">
-                        <input id="form[meaning]" name="form[meaning]" className="form-control clean-input" placeholder="Meaning"/>
+                        <input value={addMeaning} onChange={e => setAddMeaning(e.target.value)} id="form[meaning]" name="form[meaning]" className="form-control clean-input" placeholder="Meaning"/>
                     </div>
                     <div className="col-md-2">
-                        <button className="btn btn-dark w-100">Add</button>
+                        <button className="btn btn-dark w-100" onClick={new_word}>Add</button>
                     </div>
                 </div>
             </div>
